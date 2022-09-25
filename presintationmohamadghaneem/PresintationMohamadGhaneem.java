72336badce2581c6e3c59a03c7b7f62f0bb21c5f
Noor Jallad <engnoorjallad@gmail.com>
2022-09-25 21:51:17 +0300

    Add files via upload

---
+++ presintationmohamadghaneem/PresintationMohamadGhaneem.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presintationmohamadghaneem;
+
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import com.sun.xml.internal.ws.api.message.Message;
+import java.util.Properties;
+import sun.rmi.transport.Transport;
+import java.util.*;  
+ 
+
+/**
+ *
+ * @author Asus
+ */
+public class PresintationMohamadGhaneem {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String [] args){  
+   
+   }  
+}    
+    
+
